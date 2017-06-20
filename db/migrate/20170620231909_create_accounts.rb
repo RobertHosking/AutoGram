f713c017ee0ac299ca45d2164ddc076805cb649f
Robert Hosking <thedubiouslimabean@hotmail.com>
2017-06-20 23:28:42 +0000

    base application

---
+++ db/migrate/20170620231909_create_accounts.rb
@@ -0,0 +1,15 @@
+class CreateAccounts < ActiveRecord::Migration
+  def change
+    create_table :accounts do |t|
+      t.string :username
+      t.string :password
+      t.text :tags
+      t.text :comments
+      t.integer :comment_percentage
+      t.string :instagram_token
+      t.integer :category_id
+
+      t.timestamps null: false
+    end
+  end
+end
